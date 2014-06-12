d9a99df1472a00ab899f497227a87d838b83e21c
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-12 22:24:50 +0800

    Initial Commit

---
+++ RomanNumeralsKata/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RomanNumeralsKata
+//
+//  Created by Ealden Escañan on 6/12/14.
+//  Copyright (c) 2014 Ealden Escañan. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RomanNumeralsKata/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  RomanNumeralsKata
+//
+//  Created by Ealden Escañan on 6/12/14.
+//  Copyright (c) 2014 Ealden Escañan. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
