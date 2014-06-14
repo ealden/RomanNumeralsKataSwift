688e3d1218c1a5716f1274a5f0e750a367d81a9f
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-14 22:07:53 +0800

    Group related tests together

--- RomanNumeralsKataTests/RomanNumeralConverterTests.swift
+++ RomanNumeralsKataTests/RomanNumeralConverterTests.swift
@@ -10,45 +10,33 @@ import XCTest
 
 class RomanNumeralConverterTests: XCTestCase {
     let converter: RomanNumeralConverter = RomanNumeralConverter()
-    
-    func testShouldReturn1ForI() {
-        XCTAssertEqual(1, converter.convert("I"))
+
+    func testShouldReturn0ForUnsupportedRomanNumeral() {
+        XCTAssertEqual(0, converter.convert("A"))
     }
 
-    func testShouldReturn2ForII() {
-        XCTAssertEqual(2, converter.convert("II"))
+    func testShouldReturn0ForUnsupportedRomanNumeralWithSupportedOnes() {
+        XCTAssertEqual(0, converter.convert("AX"))
+    }
+
+    func testShouldReturn1ForI() {
+        XCTAssertEqual(1, converter.convert("I"))
     }
 
     func testShouldReturn5ForV() {
         XCTAssertEqual(5, converter.convert("V"))
     }
 
-    func testShouldReturn0ForUnsupportedRomanNumeral() {
-        XCTAssertEqual(0, converter.convert("A"))
-    }
-
-    func testShouldReturn4ForIV() {
-        XCTAssertEqual(4, converter.convert("IV"))
-    }
-
     func testShouldReturn10ForX() {
         XCTAssertEqual(10, converter.convert("X"))
-    }
-
-    func testShouldReturn30ForXXX() {
-        XCTAssertEqual(30, converter.convert("XXX"))
     }
 
     func testShouldReturn50ForL() {
         XCTAssertEqual(50, converter.convert("L"))
     }
-
+    
     func testShouldReturn100ForC() {
         XCTAssertEqual(100, converter.convert("C"))
-    }
-
-    func testShouldReturn300ForCCC() {
-        XCTAssertEqual(300, converter.convert("CCC"))
     }
 
     func testShouldReturn500ForD() {
@@ -59,8 +47,24 @@ class RomanNumeralConverterTests: XCTestCase {
         XCTAssertEqual(1000, converter.convert("M"))
     }
 
+    func testShouldReturn2ForII() {
+        XCTAssertEqual(2, converter.convert("II"))
+    }
+
+    func testShouldReturn30ForXXX() {
+        XCTAssertEqual(30, converter.convert("XXX"))
+    }
+
+    func testShouldReturn300ForCCC() {
+        XCTAssertEqual(300, converter.convert("CCC"))
+    }
+
     func testShouldReturn3000ForMMM() {
         XCTAssertEqual(3000, converter.convert("MMM"))
+    }
+
+    func testShouldReturn4ForIV() {
+        XCTAssertEqual(4, converter.convert("IV"))
     }
 
     func testShouldReturn9ForIX() {
@@ -169,10 +173,6 @@ class RomanNumeralConverterTests: XCTestCase {
 
     func testShouldReturn0ForXM() {
         XCTAssertEqual(0, converter.convert("XM"))
-    }
-
-    func testShouldReturn0ForUnsupportedRomanNumeralWithSupportedOnes() {
-        XCTAssertEqual(0, converter.convert("AX"))
     }
 
     func testShouldReturn0ForMultipleSubtractionIIV() {
